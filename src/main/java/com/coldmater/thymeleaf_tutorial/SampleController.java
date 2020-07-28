1e543f9390a78e8d60ba51d967df800f966e53bc
coldMater <coldmater@gmail.com>
2020-07-29 03:44:35 +0900

    국제화 테스트(messages.properties 파일과 & #{...} - thymeleaf message expression)

---
+++ src/main/java/com/coldmater/thymeleaf_tutorial/SampleController.java
@@ -0,0 +1,18 @@
+package com.coldmater.thymeleaf_tutorial;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Properties;
+
+@Controller
+public class SampleController {
+
+    @GetMapping("/sample")
+    public String sample(Model model){
+        return "sample";
+    }
+}
