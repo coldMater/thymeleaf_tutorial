4db0bce6ddbd9a00cd42ec6e07360aa76442c722
coldMater <coldmater@gmail.com>
2020-07-29 04:17:58 +0900

    Expression Utility Object 활용해보기
    ${#strings.append('날짜 ', today)}

--- src/main/java/com/coldmater/thymeleaf_tutorial/SampleController.java
+++ src/main/java/com/coldmater/thymeleaf_tutorial/SampleController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Properties;
 
 @Controller
@@ -14,6 +16,7 @@ public class SampleController {
     @GetMapping("/sample")
     public String sample(Model model){
         model.addAttribute("name", "chanwool");
+        model.addAttribute("today", LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE));
         return "sample";
     }
 }
