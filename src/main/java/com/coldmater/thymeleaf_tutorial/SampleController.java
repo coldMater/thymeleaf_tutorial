6ddda0d6842a6e49b959a9810a241ff3f0bf627d
coldMater <coldmater@gmail.com>
2020-07-29 04:45:45 +0900

    PathVariable & QueryString Access to URL

--- src/main/java/com/coldmater/thymeleaf_tutorial/SampleController.java
+++ src/main/java/com/coldmater/thymeleaf_tutorial/SampleController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -22,6 +24,18 @@ public class SampleController {
         model.addAttribute(user);
 
         return "sample";
+    }
+
+    @GetMapping("/another")
+    public String anotherQS(Model model, @RequestParam Long id){
+        model.addAttribute("id", id);
+        return "another";
+    }
+
+    @GetMapping("/another/{pathId}")
+    public String anotherPV(Model model, @PathVariable String pathId){
+        model.addAttribute("pathId", pathId);
+        return "another";
     }
 }
 
